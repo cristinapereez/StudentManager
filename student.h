abbb7cc2bae2f2e2d744dbe529963e15d050a88e
cristinapereez <cperez58@mail.csuchico.edu>
2018-05-01 11:26:06 -0700

    Create .h file

---
+++ student.h
@@ -0,0 +1,18 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student
+{
+private:
+string firstName;
+string lastName;
+public:
+void setName(string,string);
+void fullName();
+};
+
+
+
+
+
+#endif
